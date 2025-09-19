181bb05f0f6192969c1361e7d779dbebe1bdef5c
Julia-Hertseva <gerceva@gmail.com>
2025-09-19 22:02:13 +0300

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,10 +1,61 @@
 'use strict';
 
+// const { callback } = require('chart.js/helpers');
+
 /**
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+  const calcResult = {
+    get result() {
+      return result;
+    },
+    add,
+    subtract,
+    multiply,
+    divide,
+    reset,
+    operate,
+  };
+
+  function operate(callback, num) {
+    result = callback(num);
+
+    return calcResult;
+  }
+
+  function add(num) {
+    result += num;
+
+    return result;
+  }
+
+  function subtract(num) {
+    result = result - num;
+
+    return result;
+  }
+
+  function divide(num) {
+    result = result / num;
+
+    return result;
+  }
+
+  function multiply(num) {
+    result = result * num;
+
+    return result;
+  }
+
+  function reset() {
+    result = 0;
+
+    return calcResult;
+  }
+
+  return calcResult;
 }
 
 module.exports = makeCalculator;
